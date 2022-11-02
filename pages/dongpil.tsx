9feee891b05fe626e00e6971aa9ef1c8c691fce7
sj <sjc0829@naver.com>
2022-11-02 20:29:45 +0900

    [MOD] dongpil test page add

---
+++ pages/dongpil.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const DongPil: NextPage = () => {
+    return(
+        <section className="bg-slate-400">
+            <div className="">테스트 화면</div>
+        </section>
+    );
+
+}
+
+export default DongPil;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+
+  const router = useRouter();
+  console.log(router);
+
   return (
     <div className="bg-slate-400 py-20 px-10 flex flex-col space-y-5 grid gap-10">
       <div className="bg-white p-10 rounded-2xl shadow-xl">
@@ -24,7 +29,9 @@ const Home: NextPage = () => {
       </div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+          <button onClick={() => router.push("dongpil")}>동필 테스트 페이지 이동</button>
+      </div>
     </div>
   );
   
